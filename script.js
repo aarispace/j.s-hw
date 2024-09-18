96e5ae458998aaff21a4c49fb73a5c4022de32ed
aarispace <aarispace@gmail.com>
2024-09-18 17:05:42 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+ var firstvariable="welcome"
+    var firstnumber = 30
+    var secondnumber = 15
+    var thirdnumber = firstnumber*secondnumber
+    document.getElementById("result").innerHTML=firstvariable
+    document.getElementById("multiply-result").innerHTML = thirdnumber
+
